35bd74c122b897d6b23a8032ac252bf8de61a791
tryph <129980588+tryphaniel@users.noreply.github.com>
2023-11-29 18:48:19 +0800

    Update Showname.gd

--- scripts/Showname.gd
+++ scripts/Showname.gd
@@ -1,8 +1,12 @@
 @tool
 extends PanelContainer
-
+## Displays the [String] in the Name Box.
+##
+## Changes the name of the [code]speaker[/code] on the name box[br]
+## for the current text.
 @onready var rtf_label = $MarginContainer/HBoxContainer/RichTextLabel
 
+## Displays the [String] in the name box.
 @export var showname: String = "":
 	get:
 		if rtf_label:
